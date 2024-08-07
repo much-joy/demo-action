81480a9dcc3d5fb7f85485937122cba7d5e60242
欢乐多 <>
2024-08-07 15:22:15 +0800

    提交测试类

---
+++ src/test/java/com/action/DemoActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
